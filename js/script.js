568b089c716ce1264f4362a90eaa5652dc34920d
IsaacDesire <87551430+IsaacDesire@users.noreply.github.com>
2022-05-16 13:07:47 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// Function 
+function validateForm() {
+	
+	// 3 variables  
+	
+	var name = document.myform.name.value;
+	var email = document.myform.email.value;
+	var message = document.myform.message.value;
+	
+	
+	// Conditions 
+	
+	if (name == null || name == "") {
+		alert("Name can't be blank");
+		return false;
+	}
+	else if (email == null || email == "") {
+		alert("Email can't be blank");
+		return false;
+	}
+	else if (message == null || message == "") {
+		alert("Message can't be blank");
+		return false;
+	}	
+}
